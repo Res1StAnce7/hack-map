853d1489e476abaa3c63b6e9502c6fe86872c924
Siliang Zhang <89612705+Res1StAnce7@users.noreply.github.com>
2023-02-05 00:29:26 -0500

    fix location update & add event windows

--- src/Map.js
+++ src/Map.js
@@ -2,16 +2,8 @@ import React, {useEffect} from 'react'
 import { useRef } from 'react'
 import {GoogleMap, useJsApiLoader, Autocomplete,
     DirectionsRenderer, Marker, HeatmapLayer} from '@react-google-maps/api';
-import {
-    Box,
-    Button,
-    ButtonGroup,
-    Flex,
-    HStack,
-    IconButton,
-    Input,
-    Text,
-    Modal, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, ModalContent, ModalFooter,
+import {Box, Button, ButtonGroup, Flex, HStack, IconButton, Input, Text,
+    Modal, ModalOverlay, ModalHeader, ModalCloseButton, ModalBody, ModalContent, Textarea,
 } from '@chakra-ui/react'
 import {FaLocationArrow, FaMap, FaTimes} from 'react-icons/fa'
 import {RiAlarmWarningFill} from 'react-icons/ri'
@@ -32,37 +24,21 @@ function MyComponent() {
         googleMapsApiKey: "AIzaSyCbibYU79CUknzovDk7S1ZGoBF9oCeEx9Y",
         libraries:['places', 'directions', 'geometry', 'visualization']
     })
-    const [showWindow, setShowWindow] = React.useState(false)
-    const [center, setCenter] = React.useState({ lat: null, lng: null })
+    const [showWindowMain, setShowWindowMain] = React.useState(false)
+    const [showWindowSub1, setShowWindowSub1] = React.useState(false)
+    const [showWindowSub2, setShowWindowSub2] = React.useState(false)
+    const [showWindowSub3, setShowWindowSub3] = React.useState(false)
+    const [center, setCenter] = React.useState({lat: 40.756219, lng: -73.98641})
     const [map, setMap] = React.useState(null)
     const [distance, setDistance] = React.useState('--')
     const [duration, setDuration] = React.useState('--')
     const [directionsResponse, setDirectionsResponse] = React.useState(null)
     const [showHeatmap, setShowHeatmap] = React.useState(false);
-
     /** @type React.MutableRefObject<HTMLInputElement> */
     const originRef = useRef()
     /** @type React.MutableRefObject<HTMLInputElement> */
     const destinationRef = useRef()
     const heatmapRef = useRef();
-
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    setCenter({
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude,
-                    });
-                },
-                (error) => {
-                    console.error("Error: ", error);
-                }
-            );
-        } else {
-            console.error("Geolocation is not supported by this browser.");
-        }
-    }, []);
 
 
     async function calculateRoute() {
@@ -92,40 +68,38 @@ function MyComponent() {
 
     function makeAndSetData() {
         if (!data) {
-            for (let i = 0; i < 10000; i++) {
+            let file = require('./data.json');
+            file.forEach((item) => {
                 heatMapData.push({
                     // eslint-disable-next-line no-undef
-                    location: new google.maps.LatLng(
-                        center.lat + 2 * ((Math.random() > 0.5) ? 1 : -1) * Math.random() / 100,
-                        center.lng + 2 * ((Math.random() > 0.5) ? 1 : -1) * Math.random() / 100
-                    ),
-                    weight: Math.random() * 100,
+                    location: new google.maps.LatLng(item.lat, item.lng),
+                    weight: Math.random() * 1000,
                 });
-            }
+            });
             data = true;
         }
         return heatMapData;
     }
 
+    function setCurrentLocation() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setCenter({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+            })
+        })
+        map.panTo(center)
+    }
+
     return isLoaded ? (
-        <Flex
-            position='relative'
-            flexDirection='column'
-            alignItems='center'
-            h='100vh'
-            w='100vw'
-        >
+        <Flex position='relative' flexDirection='column' alignItems='center' h='100vh' w='100vw'>
             <Box position={'absolute'} left={0} top={0} h={'100%'} w={'100%'}>
-                <GoogleMap
-                    mapContainerStyle={containerStyle}
-                    center={center}
-                    zoom={17}
-                    // onLoad={onLoad}
-                    onLoad={map => setMap(map)}
+                <GoogleMap mapContainerStyle={containerStyle} center={center}
+                    zoom={17} onLoad={map => setMap(map)}
                     options={{
-                        mapTypeControl: false
-                    }}
-                >
+                        mapTypeControl: false,
+                        streetViewControl: false
+                    }}>
                     {showHeatmap && <HeatmapLayer data={makeAndSetData()} ref={heatmapRef}/>}
                     {center.lat !== null && center.lng !== null && (
                         <Marker position={center}
@@ -137,22 +111,10 @@ function MyComponent() {
                     {directionsResponse && (
                         <DirectionsRenderer directions={directionsResponse} />
                     )}
-                    { /* Child components, such as markers, info windows, etc. */ }
-                    <></>
                 </GoogleMap>
             </Box>
-            <Box
-                p={5}
-                borderRadius={"2xl"}
-                m={5}
-                bgColor='#FBFBF5'
-                shadow='1px 2px 9px #ADD8E6'
-                minW='container.md'
-                zIndex='1'
-                position={'absolute'}
-                top={'0px'}
-                left={'0px'}
-            >
+            <Box p={5} borderRadius={"2xl"} m={5} bgColor='#FBFBF5' shadow='1px 2px 9px #ADD8E6'
+                minW='container.md' zIndex='1' position={'absolute'} top={'0px'} left={'0px'}>
                 <HStack spacing={2} justifyContent='space-between'>
                     <Box flexGrow={1} shadow={'sm'}>
                         <Autocomplete>
@@ -178,53 +140,86 @@ function MyComponent() {
                 <HStack spacing={4} mt={4} justifyContent='space-between'>
                     <Text>Distance: {distance} </Text>
                     <Text>Duration: {duration} </Text>
-                    <IconButton
-                        aria-label='center back'
-                        icon={<RiAlarmWarningFill />}
-                        onClick={() => setShowWindow(true)}
-                    >
-                    </IconButton>
-                    <IconButton
-                        aria-label='center back'
-                        icon={<FaLocationArrow />}
-                        isRound
-                        onClick={() => {
-                            map.panTo(center)
-                            map.setZoom(17)
-                        }}
-                    />
-                    <IconButton
-                        aria-label={'center back'}
-                        icon={<FaMap />}
-                        isRound
-                        onClick={() => setShowHeatmap(!showHeatmap)}/>
+                    <ButtonGroup>
+                        <IconButton
+                            aria-label='center back'
+                            icon={<RiAlarmWarningFill />}
+                            onClick={() => setShowWindowMain(!showWindowMain)}
+                        >
+                        </IconButton>
+                        <IconButton
+                            aria-label='center back'
+                            icon={<FaLocationArrow />}
+                            isRound
+                            onClick={() => setCurrentLocation()}
+                        />
+                        <IconButton
+                            aria-label={'center back'}
+                            icon={<FaMap />}
+                            isRound
+                            onClick={() => setShowHeatmap(!showHeatmap)}/>
+                    </ButtonGroup>
                 </HStack>
             </Box>
-            {showWindow && (
-                <Modal isOpen={showWindow} onClose={() => setShowWindow(false)}>
+            {showWindowMain && (
+                <Modal isOpen={showWindowMain} onClose={() => setShowWindowMain(false)}>
                     <ModalOverlay />
-                    <ModalContent style={{textAlign: "center"}}>
+                    <ModalContent style={{textAlign: "center", width: "500px", height: "500px"}}>
                         <ModalHeader>Modal Title</ModalHeader>
                         <ModalCloseButton />
                         <ModalBody style={{display: "flex", flexDirection: "column"}}>
-                            <Button variant="ghost">1</Button>
-                            <Button variant="ghost">2</Button>
-                            <Button variant="ghost">3</Button>
-                            <Button variant="ghost">4</Button>
+                            <Button variant="ghost" color="primary" style={{height: "90px"}}
+                                    onClick={() => setShowWindowSub1(!showWindowSub1)}>
+                                1
+                            </Button>
+                            <Button variant="ghost" style={{marginTop: "50px", height: "90px"}}
+                                    onClick={() => setShowWindowSub2(!showWindowSub2)}>
+                                2
+                            </Button>
+                            <Button variant="ghost" style={{marginTop: "50px", height: "90px"}}
+                                onClick={() => setShowWindowSub3(!showWindowSub3)}>
+                                3
+                            </Button>
                         </ModalBody>
-                        <ModalFooter>
-                            {/*<Button colorScheme="blue" mr={3} onClick={() => setShowWindow(!showWindow)}>*/}
-                            {/*    Close*/}
-                            {/*</Button>*/}
-                            {/*<Button variant="ghost">Secondary Action</Button>*/}
-                        </ModalFooter>
                     </ModalContent>
                 </Modal>
             )}
+            {showWindowSub1 && (<Modal isOpen={showWindowSub1} onClose={() => setShowWindowSub1(false)}>
+                <ModalOverlay />
+                <ModalContent style={{textAlign: "center", width: "500px", height: "500px"}}>
+                    <ModalCloseButton />
+                    <ModalHeader>Report</ModalHeader>
+                    <ModalBody style={{display: "flex", flexDirection: "column"}}>
+                        <Input placeholder="Title" />
+                        <Textarea placeholder="Description" style={{marginTop: "20px", height: "320px"}}/>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>)}
+            {showWindowSub2 && (<Modal isOpen={showWindowSub2} onClose={() => setShowWindowSub2(false)}>
+                <ModalOverlay />
+                <ModalContent style={{textAlign: "center", width: "500px", height: "500px"}}>
+                    <ModalCloseButton />
+                    <ModalHeader>Notify</ModalHeader>
+                    <ModalBody style={{display: "flex", flexDirection: "column"}}>
+                        <Input placeholder="Title" />
+                        <Textarea placeholder="Description" style={{marginTop: "20px", height: "320px"}}/>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>)}
+            {showWindowSub3 && (<Modal isOpen={showWindowSub3} onClose={() => setShowWindowSub3(false)}>
+                <ModalOverlay />
+                <ModalContent style={{textAlign: "center", width: "500px", height: "500px"}}>
+                    <ModalCloseButton />
+                    <ModalHeader>Call 911</ModalHeader>
+                    <ModalBody style={{display: "flex", flexDirection: "column"}}>
+                        <Text style={{marginTop:"150px"}}>Police is on the way</Text>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>)}
         </Flex>
     ) : <></>
 }
 
 export default React.memo(MyComponent)
 
-//AIzaSyCbibYU79CUknzovDk7S1ZGoBF9oCeEx9Y
+//APIKey: AIzaSyCbibYU79CUknzovDk7S1ZGoBF9oCeEx9Y
